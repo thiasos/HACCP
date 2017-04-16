7d0e32960101446e2780fb362633a96a940b6422
Bruno Lombardi <b.lombardi@mclink.eu>
2017-04-16 22:07:36 +0200

    init man project

---
+++ spring-boot/src/main/java/it/lsoft/haccp/HaccpRegistryApplication.java
@@ -0,0 +1,12 @@
+package it.lsoft.haccp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaccpRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaccpRegistryApplication.class, args);
+	}
+}
---
+++ spring-boot/src/test/java/it/lsoft/haccp/HaccpRegistryApplicationTests.java
@@ -0,0 +1,16 @@
+package it.lsoft.haccp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HaccpRegistryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
