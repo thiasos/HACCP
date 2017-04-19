a3f42631ed2fca566203a4727d47327d0ee2744b
Bruno Lombardi <b.lombardi@mclink.eu>
2017-04-19 18:08:16 +0200

    Table listing

--- spring-boot/src/main/resources/public/js/controller.js
+++ spring-boot/src/main/resources/public/js/controller.js
@@ -1,13 +1,21 @@
 'use strict';
 angular.module('MyApp', [ 'ngMaterial', 'ngMessages', 'md.data.table' ])
-		.controller('HaccpCtrl', HaccpCtrl).factory('providerService',
-				providerService);
-function providerService($http, $log, $q) {
+		.controller('HaccpCtrl', HaccpCtrl)//
+		.factory('crudService', crudService)//
+		.factory('providerService', providerService);
+function providerService($http, $log, $q, crudService) {
 	return {
-		listProviders : function(page, size) {
+		list : function(page, size, sortColumns, sortOrder) {
+			return crudService.list('fornitoris', page, size, sortColumns, sortOrder)
+		}
+	}
+}
+function crudService($http, $log, $q) {
+	return {
+		list : function(servicename, page, size, sortColumns, sortOrder) {
 			var deferred = $q.defer();
-			$http.get('fornitoris?page=' + (page-1)+"&size="+size).then(
-					function(result) {
+			$http.get(servicename + '?page=' + (page - 1) + "&size=" + size+'&sort='+sortColumns +','+sortOrder)
+					.then(function(result) {
 						deferred.resolve(result.data);
 					}, function(msg, code) {
 						deferred.reject(msg);
@@ -21,13 +29,30 @@ function providerService($http, $log, $q) {
 function HaccpCtrl($timeout, $http, $q, $log, providerService, $scope) {
 	var self = this;
 	self.listProviders = listProviders;
+	self.sortProviders = sortProviders;
+	self.nextProviders = nextProviders;
 	self.provider = new Object();
-	function listProviders(page, size) {
-		self.provider.promise=  providerService.listProviders(page, size);
-		self.provider.promise.then(function (result) {
-			self.provider.result = result;
-			self.provider.result.page.number = self.provider.result.page.number+1;
-		});
+ 
+	function sortProviders(sortColumns) {
+		self.listProviders(self.provider.result.page.number, self.provider.result.page.size, sortColumns)
 	}
-	listProviders(1,10);
+	function nextProviders(page, size) {
+		self.listProviders(page, size, self.provider.order)
+	}
+	function listProviders(page, size, sortColumn) {
+		var sortOrder = 'asc';
+		if (sortColumn !=undefined && sortColumn.startsWith("-")) {
+			  sortOrder = 'desc';	
+			  sortColumn = sortColumn.substr(1, sortColumn.length-1);
+		}
+		self.provider.promise = providerService.list(page, size, sortColumn, sortOrder);
+		self.provider.promise
+				.then(function(result) {
+					self.provider.result = result;
+					self.provider.result.page.number = self.provider.result.page.number + 1;
+				});
+	}
+	self.provider.order = 'id';
+	listProviders(1, 10);
+	
 }
